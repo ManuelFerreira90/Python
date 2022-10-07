0ace0a78955ae13bc06e0a1d280b5b150973fc06
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2022-10-07 06:43:40 -0300

    Novos Exercícios

---
+++ Revisão II/Ex18.py
@@ -0,0 +1,12 @@
+#18
+altjuca = 1.10
+altchico = 1.50
+contadoranos = 0
+while altjuca <= altchico:
+      if altjuca <= altchico:
+         altchico += 0.02
+         altjuca += 0.03
+         contadoranos += 1
+      else:
+        break 
+print(f"Necessários {contadoranos} anos")
