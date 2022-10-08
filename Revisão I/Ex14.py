f8e1627919df8d4d2d47156d4cbb30c3e2529386
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2022-10-08 12:33:50 -0300

    Novos exercícios

---
+++ Revisão I/Ex14.py
@@ -0,0 +1,9 @@
+#14
+numero = 0
+while numero != -999:
+      numero = float(input())
+      if(numero != -999):
+          triplo = numero * 3
+          print("O triplo é: ",triplo) 
+      else:
+          break
