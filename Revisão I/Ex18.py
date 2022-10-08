9f647ba2f7510b6fb43a42810125a42316e509b8
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2022-10-08 12:34:01 -0300

    Novos exercícios

---
+++ Revisão I/Ex18.py
@@ -0,0 +1,14 @@
+#18
+N = int(input())
+primeirotermo = 1
+segundotermo = 4
+terceirotermo = 4
+
+
+for i in range(1,(N+1)):
+   print(primeirotermo, segundotermo, terceirotermo, end=" ")
+   primeirotermo +=1
+   segundotermo +=1
+   terceirotermo +=1
+  
+        
