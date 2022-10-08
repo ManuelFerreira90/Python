2bf4c7d1fafaefdffcec1d6c0c9657568c0dd93f
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2022-10-08 12:34:13 -0300

    Novos exercícios

---
+++ Revisão I/Ex20.py
@@ -0,0 +1,12 @@
+#20
+massa = float(input())
+tempo = 0
+massafinal = 0 
+
+while massa >= 0.10:
+    if massa >= 0.10:
+       massa = massa * 0.75
+       tempo += 30
+    else:
+        break
+print(tempo," segundos")
